e91d411ec449d03ce979abb1adc375194cde3740
lencho_Ahmed <mandeveloper582@gmail.com>
2026-09-03 23:16:26 +0300

    fix: Fix case-sensitivity issue in useTheme.js import
    - Fix import path from './ThemeContext' to './Themecontext'
    - Resolves secondary import error on case-sensitive file systems

--- src/context/useTheme.js
+++ src/context/useTheme.js
@@ -1,4 +1,4 @@
 import { useContext } from "react";
-import { ThemeContext } from "./ThemeContext";
+import { ThemeContext } from "./Themecontext";
 
 export const useTheme = () => useContext(ThemeContext);
